0c579312db94943842c700af15616cdf236068dc
Niccolo Rojas <niccolo.rojas.ge@gmail.com>
2026-01-22 18:03:58 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
 import { useState } from 'react'
 
+import AppArticle from './components/AppArticle'
+
+const originalArticles = [
+  'Come fare la pasta',
+  'Come fare la spesa',
+];
 
 function App() {
-  const [write, setWrite] = useState("")
-  const formSend = event => {
-    event.preventDefault();
-    
-  }
+  console.log("Rendering di App.jsx")
+  const [articles, setArticles] = useState(originalArticles)
+  const [newPost, setNewPost] = useState("")
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setArticles([ ...articles, newPost ]);
+    setNewPost("");
+  } 
 
   return (
     <>
-      <form onSubmit={formSend} action="">
-        <input type="text"
-          id="titolo"
-          value={write}
-          placeholder='inserisci articoli del blog'
-          onChange={e => { setWrite(e.target.value) }}
-        />
-        <p>{write}</p>
-        <button>invia</button>
-      </form>
+    <div>
+    {
+      articles.map( (article, i) => <AppArticle key={i} title={article} /> )
+    }
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={newPost} onChange={(e) => setNewPost(e.target.value)} />
+      <input type="submit" value="aggiungi nuovo post" />
+    </form>
+    <AppArticle title="PROVA TITOLO" />
+    </div>
     </>
   )
 }
---
+++ src/components/AppArticle.jsx
@@ -0,0 +1,9 @@
+function AppArticle({ title }) {
+    return (
+        <div className="card">
+            <h2>{title}</h2>
+        </div>
+    )
+}
+
+export default AppArticle
