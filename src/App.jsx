62a26d969865f938011385b4ccea1c87c2f179b3
Niccolo Rojas <niccolo.rojas.ge@gmail.com>
2026-01-22 16:14:12 +0100

    form

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,25 @@ import { useState } from 'react'
 
 
 function App() {
-  
+  const [write, setWrite] = useState("")
+  const formSend = event => {
+    event.preventDefault();
+    
+  }
+
 
   return (
     <>
-
+      <form onSubmit={formSend} action="">
+        <input type="text"
+          id="titolo"
+          value={write}
+          placeholder='inserisci articoli del blog'
+          onChange={e => { setWrite(e.target.value) }}
+        />
+        <p>{write}</p>
+        <button>invia</button>
+      </form>
     </>
   )
 }
